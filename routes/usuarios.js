b6bd27158a687711fd6e88172952874d28a17400
gusmorini <gustavommh@gmail.com>
2018-08-18 14:24:32 -0300

    implementou o update

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -13,23 +13,71 @@ router.post('/', (request, response) => {
         nome, email, nascimento
     })
         .then (usuario => {
-
             response
                 .status(201)
                 .json(usuario)
-
         })
 
         .catch ( ex => {
-            
             console.error(ex);
             response
                 .status(412)
                 .send('não foi possível incluir o registro');
+        })
+});
 
+router.get('/:usuarioId', (request, response) => {
+
+    const { params } = request
+    const { usuarioId } = params
+
+    Usuario.findById(usuarioId)
+        .then( usuario => {
+            if (!usuario)
+            {
+                response.status(404).send('Usuário não encontrado')
+            }
+            else
+            {
+                response.status(200).json(usuario)
+            }
+        })
+        .catch( ex => {
+            console.error(ex);
+            response.status(412).send('Não foi possivel consultar o banco de dados')
         })
 
+})
 
-});
+router.put("/:usuarioId", (request, response) => {
+
+    // const { params, body } = request
+    // const { usuarioId } = params
+    // const { nome, email, nascimento } = body
+
+    const { params: {usuarioId}, body: { nome, email, nascimento} } = request
+
+    Usuario.findById(usuarioId)
+        .then( usuario => {
+            if (!usuario)
+            {
+                response.status(404).send('Usuário não encontrado')
+            }
+            else
+            {
+                return usuario.update({
+                    nome, email, nascimento
+                })
+                .then(()=>{
+                    response.status(200).json(usuario)
+                })
+            }
+        })
+        .catch( ex => {
+            console.error(ex);
+            response.status(412).send('Não foi possivel consultar o banco de dados')
+        })
+
+})
 
 module.exports = router;
